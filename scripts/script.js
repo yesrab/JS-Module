9ed130e74eafada4b7f8e96b0cee9acaf5eb2f87
yesrab <yesrab.ali111@gmail.com>
2023-10-10 19:54:45 +0530

    initial build

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("Test");
+const rulesWindow = document.getElementById("rules");
+function exitRules() {
+  rulesWindow.style.display = "none";
+}
+function openRules() {
+  rulesWindow.style.display = "block";
+}
+function placeholder() {
+  alert("the is a place holder");
+}
